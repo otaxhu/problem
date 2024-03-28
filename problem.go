a59f7cd5e9009e281d55065c79facd4408bfb186
otaxhu <oscarperniamoreno@gmail.com>
2024-03-28 00:10:50 -0400

    BREAKING CHANGES: changed NewProblem to NewDefault and added a new omitEmpty optional parameter
    feat: added a new field to Problem struct OmitEmpty, bool flag indicating if the other fields during JSON un/marshall are ommited if they are zero-values
    
    feat: error proof functions and less panics

--- problem.go
+++ problem.go
@@ -2,6 +2,7 @@ package problem
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -13,15 +14,20 @@ type Problem struct {
 	Detail           string
 	Instance         string
 	ExtensionMembers map[string]any
+
+	OmitEmpty bool
 }
 
-func NewProblem(statusCode int) Problem {
+func NewDefault(statusCode int, omitEmpty ...bool) Problem {
 	if val, ok := problems[statusCode]; ok {
 		val.Status = statusCode
 		val.ExtensionMembers = map[string]any{}
+		if len(omitEmpty) > 0 {
+			val.OmitEmpty = omitEmpty[0]
+		}
 		return val
 	}
-	panic(fmt.Sprintf("problem.go: Invalid '%03d' status code at NewProblem() function", statusCode))
+	panic(fmt.Sprintf("problem.go: Invalid '%03d' status code at NewDefault() function", statusCode))
 }
 
 func (p *Problem) AddExtensionMember(key string, value any) {
@@ -41,31 +47,76 @@ func (p Problem) MarshalJSON() ([]byte, error) {
 			m[k] = v
 		}
 	}
-	if p.Type != "" {
+	if p.Type != "" || !p.OmitEmpty {
 		m["type"] = p.Type
 	}
-	if ok := http.StatusText(p.Status); ok != "" {
+	if p.Status != 0 || !p.OmitEmpty {
 		m["status"] = p.Status
-	} else {
-		panic(fmt.Sprintf("problem.go: Invalid '%03d' status code at (Problem).MarshalJSON() function", p.Status))
 	}
-	if p.Title != "" {
+	if p.Title != "" || !p.OmitEmpty {
 		m["title"] = p.Title
 	}
-	if p.Detail != "" {
+	if p.Detail != "" || !p.OmitEmpty {
 		m["detail"] = p.Detail
 	}
-	if p.Instance != "" {
+	if p.Instance != "" || !p.OmitEmpty {
 		m["instance"] = p.Instance
 	}
 	return json.Marshal(m)
 }
 
+// Just implemented for convinience in tests, Unmarshals the buffer to the Problem struct
+func (p *Problem) UnmarshalJSON(b []byte) error {
+	var x any
+	if err := json.Unmarshal(b, &x); err != nil {
+		return err
+	}
+	m, ok := x.(map[string]any)
+	if !ok {
+		return errors.New("problem.go: Cannot unmarshall the JSON to the Problem struct, x is not a map[string]any")
+	}
+	for k, v := range m {
+		switch k {
+		case "type":
+			val, ok := v.(string)
+			if !ok {
+				return errors.New("problem.go: type of 'type' JSON member is not string")
+			}
+			p.Type = val
+		case "status":
+			val, ok := v.(float64)
+			if !ok {
+				return errors.New("problem.go: type of 'status' JSON member is not float64")
+			}
+			p.Status = int(val)
+		case "title":
+			val, ok := v.(string)
+			if !ok {
+				return errors.New("problem.go: type of 'title' JSON member is not string")
+			}
+			p.Title = val
+		case "detail":
+			val, ok := v.(string)
+			if !ok {
+				return errors.New("problem.go: type of 'detail' JSON member is not string")
+			}
+			p.Detail = val
+		case "instance":
+			val, ok := v.(string)
+			if !ok {
+				return errors.New("problem.go: type of 'instance' JSON member is not string")
+			}
+			p.Instance = val
+		default:
+			p.ExtensionMembers[k] = v
+		}
+	}
+	return nil
+}
+
 func (p Problem) Send(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/problem+json")
-	if ok := http.StatusText(p.Status); ok == "" {
-		panic(fmt.Sprintf("problem.go: Invalid '%03d' status code at (Problem).Send() function", p.Status))
-	}
+	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(p.Status)
 	json.NewEncoder(w).Encode(p)
 }
