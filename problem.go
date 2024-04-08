cddce5187842fe4a5c1b1d0ab2b36bbe371f19ec
otaxhu <oscarperniamoreno@gmail.com>
2024-04-07 20:57:25 -0400

    fix: NewDefault now creates Problem with nil map
    feat: docs about all functions and types

--- problem.go
+++ problem.go
@@ -7,6 +7,12 @@ import (
 	"net/http"
 )
 
+// Problem details struct, you can get an instance with default values through "NewDefault"
+// constructor, or you can build your own Problem by instatiate it yourself and set the wanted
+// fields.
+//
+// Then you can use (Problem).Send() method to send the Problem details through a
+// http.ResponseWriter
 type Problem struct {
 	Type             string
 	Status           int
@@ -18,10 +24,13 @@ type Problem struct {
 	OmitEmpty bool
 }
 
+// Builds a new Problem with default values, if "statusCode" is an invalid HTTP status code, then
+// function panics.
+//
+// "omitEmpty" param if present and true, omits fields with zero values during JSON marshaling.
+// Can be setted again to another value through "OmitEmpty" struct field
 func NewDefault(statusCode int, omitEmpty ...bool) Problem {
 	if val, ok := problems[statusCode]; ok {
-		val.Status = statusCode
-		val.ExtensionMembers = map[string]any{}
 		if len(omitEmpty) > 0 {
 			val.OmitEmpty = omitEmpty[0]
 		}
@@ -30,6 +39,10 @@ func NewDefault(statusCode int, omitEmpty ...bool) Problem {
 	panic(fmt.Sprintf("problem.go: Invalid '%03d' status code at NewDefault() function", statusCode))
 }
 
+// Adds "key" and "value" to the "ExtensionMembers" map, if "key" already exists in map, then "key"
+// is overwrited.
+//
+// If "p.ExtensionMembers" is nil, a new map will be created and the step above will be performed
 func (p *Problem) AddExtensionMember(key string, value any) {
 	if _, isStandard := standardMembers[key]; isStandard {
 		return
@@ -40,6 +53,11 @@ func (p *Problem) AddExtensionMember(key string, value any) {
 	p.ExtensionMembers[key] = value
 }
 
+// Implements json.Marshaler interface
+//
+// NOTE:
+// Should not be used in application code, instead you should use (json.Encoder).Encode() method or
+// json.Marshal() function to encode the Problem struct
 func (p Problem) MarshalJSON() ([]byte, error) {
 	m := make(map[string]any, 5+len(p.ExtensionMembers))
 	for k, v := range p.ExtensionMembers {
@@ -65,7 +83,10 @@ func (p Problem) MarshalJSON() ([]byte, error) {
 	return json.Marshal(m)
 }
 
-// Just implemented for convinience in tests, Unmarshals the buffer to the Problem struct
+// Implements json.Unmarshaler interface
+//
+// NOTE:
+// Just implemented for convinience in tests, unmarshals the buffer to the Problem struct
 func (p *Problem) UnmarshalJSON(b []byte) error {
 	var x any
 	if err := json.Unmarshal(b, &x); err != nil {
@@ -114,6 +135,10 @@ func (p *Problem) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// Writes "Problem" struct to "w" with "application/problem+json" as Content-Type header and status
+// code set to "p.Status" field value.
+//
+// Further calls to "w" methods should not be performed after call to this method
 func (p Problem) Send(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/problem+json")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
