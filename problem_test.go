1404ee0399ed25063021f2361be00937b82af2d5
otaxhu <oscarperniamoreno@gmail.com>
2024-10-21 00:44:44 -0400

    feat: added tests for the new API

--- problem_test.go
+++ problem_test.go
@@ -1,151 +1,190 @@
 package problem
 
-// TODO: Remade all of the tests using the new API before bumping to V1 stable
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"net/http"
-// 	"testing"
-// )
+func equalProblems(a Problem, b Problem) bool {
+	return a.GetType() == b.GetType() &&
+		a.GetTitle() == b.GetTitle() &&
+		a.GetStatus() == b.GetStatus() &&
+		a.GetInstance() == b.GetInstance() &&
+		a.GetDetail() == b.GetDetail()
+}
 
-// func compareProblems(a, b Problem) bool {
-// 	if a.Type != b.Type {
-// 		return false
-// 	}
-// 	if a.Title != b.Title {
-// 		return false
-// 	}
-// 	if a.Detail != b.Detail {
-// 		return false
-// 	}
-// 	if a.Status != b.Status {
-// 		return false
-// 	}
-// 	if a.Instance != b.Instance {
-// 		return false
-// 	}
-// 	// TODO: compare the ExtensionMembers
-// 	//
-// 	// if !maps.Equal(a.ExtensionMembers, b.ExtensionMembers) {
-// 	// 	return false
-// 	// }
-// 	return true
-// }
+func compactJson(s string) string {
+	b := bytes.Buffer{}
+	err := json.Compact(&b, []byte(s))
+	if err != nil {
+		panic(err)
+	}
+	return b.String()
+}
 
-// func TestNewDefault(t *testing.T) {
-// 	testCases := []struct {
-// 		Name       string
-// 		StatusCode int
+func TestRegisteredProblemMarshaling(t *testing.T) {
+	testCases := map[string]struct {
+		InputProblem *RegisteredProblem
+		ExpectedJSON string
 
-// 		ExpectedProblem Problem
-// 		ExpectsPanic    bool
-// 	}{
-// 		{
-// 			Name:       "Success_Status200",
-// 			StatusCode: 200,
+		// TODO: find a way to compact a XML document
+		//
+		// ExpectedMarshalXML string
+	}{
+		"OK": {
+			InputProblem: NewRegistered(http.StatusBadRequest, "test"),
+			ExpectedJSON: compactJson(`
+				{
+					"type": "about:blank",
+					"status": 400,
+					"title": "Bad Request",
+					"detail": "test",
+					"instance": ""
+				}
+			`),
+		},
+	}
 
-// 			ExpectedProblem: Problem{
-// 				Type:   "https://www.rfc-editor.org/rfc/rfc9110.html#name-200-ok",
-// 				Title:  "200 OK",
-// 				Status: 200,
-// 			},
-// 			ExpectsPanic: false,
-// 		},
-// 		{
-// 			Name:       "Fail_Status99",
-// 			StatusCode: 99,
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			b, err := json.Marshal(tc.InputProblem)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(b, []byte(tc.ExpectedJSON)) {
+				t.Errorf("expected %s, got %s", tc.ExpectedJSON, b)
+			}
 
-// 			ExpectedProblem: Problem{},
-// 			ExpectsPanic:    true,
-// 		},
-// 	}
-// 	for _, tc := range testCases {
-// 		t.Run(tc.Name, func(t *testing.T) {
-// 			defer func() {
-// 				r := recover()
-// 				if r == nil && tc.ExpectsPanic {
-// 					t.Errorf("expected NewProblem function to panic, doesn't panicked")
-// 				} else if r != nil && !tc.ExpectsPanic {
-// 					t.Errorf("expected NewProblem function not to panic, panicked")
-// 				}
-// 			}()
-// 			prob := NewDefault(tc.StatusCode)
-// 			if !compareProblems(prob, tc.ExpectedProblem) {
-// 				t.Errorf("unexpected Problem value, expected '%v' got '%v'", tc.ExpectedProblem, prob)
-// 			}
-// 		})
-// 	}
-// }
+			// TODO: see above, find a way to compact a XML document
+			//
+			// b, err = xml.Marshal(tc.InputMarshalProblem)
+			// if err != nil {
+			// 	t.Fatal(err)
+			// }
+			// if !bytes.Equal(b, []byte(tc.ExpectedMarshalXML)) {
+			// 	t.Errorf("marshal problem XML: expected %s, got %s", tc.ExpectedMarshalJSON, b)
+			// }
+		})
+	}
+}
 
-// type responseWriter struct {
-// 	bytes.Buffer
-// 	header     http.Header
-// 	statusCode int
-// }
+func TestParseResponse(t *testing.T) {
+	testCases := map[string]struct {
+		InputBody       *http.Response
+		ExpectedProblem *RegisteredProblem
+	}{
+		"JSON: OK": {
+			InputBody: &http.Response{
+				Body: io.NopCloser(strings.NewReader(`
+					{
+						"type": "about:blank",
+						"status": 500,
+						"title": "Internal Server Error",
+						"detail": "test",
+						"instance": "/test"
+					}
+				`)),
+				Header: http.Header{
+					"Content-Type": []string{problemJsonContentType},
+				},
+				StatusCode: http.StatusInternalServerError,
+			},
+			ExpectedProblem: &RegisteredProblem{
+				Type:     "about:blank",
+				Status:   http.StatusInternalServerError,
+				Title:    "Internal Server Error",
+				Detail:   "test",
+				Instance: "/test",
+			},
+		},
+		"XML: OK": {
+			InputBody: &http.Response{
+				Body: io.NopCloser(strings.NewReader(xml.Header + `
+					<problem xmlns="urn:ietf:rfc:7807">
+						<type>about:blank</type>
+						<status>500</status>
+						<title>Internal Server Error</title>
+						<detail>test</detail>
+						<instance>/test</instance>
+					</problem>
+				`)),
+				Header: http.Header{
+					"Content-Type": []string{problemXmlContentType},
+				},
+				StatusCode: http.StatusInternalServerError,
+			},
+			ExpectedProblem: &RegisteredProblem{
+				Type:     "about:blank",
+				Status:   http.StatusInternalServerError,
+				Title:    "Internal Server Error",
+				Detail:   "test",
+				Instance: "/test",
+			},
+		},
+	}
 
-// func (r *responseWriter) Header() http.Header {
-// 	if r.header == nil {
-// 		r.header = http.Header{}
-// 	}
-// 	return r.header
-// }
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			outProblem, err := ParseResponse(tc.InputBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !equalProblems(outProblem, tc.ExpectedProblem) {
+				t.Errorf("expected %+v, got %+v", tc.ExpectedProblem, outProblem)
+			}
+		})
+	}
+}
 
-// func (r *responseWriter) WriteHeader(statusCode int) {
-// 	r.statusCode = statusCode
-// }
+func TestServe(t *testing.T) {
+	testCases := map[string]struct {
+		InputProblem *RegisteredProblem
+		ExpectedJSON string
 
-// func TestProblemSend(t *testing.T) {
-// 	testCases := []struct {
-// 		Name         string
-// 		InputProblem Problem
+		// TODO: find a way to compact a XML documet
+		//
+		// ExpectedXML string
+	}{
+		"OK": {
+			InputProblem: NewRegistered(http.StatusBadRequest, "test"),
+			ExpectedJSON: compactJson(`
+				{
+					"type": "about:blank",
+					"status": 400,
+					"title": "Bad Request",
+					"detail": "test",
+					"instance": ""
+				}
+			`),
+		},
+	}
 
-// 		ExpectedProblem     Problem
-// 		ExpectedContentType string
-// 		ExpectedStatusCode  int
-// 	}{
-// 		{
-// 			Name:         "Success_NewDefault(200)",
-// 			InputProblem: NewDefault(200),
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			recorder := httptest.NewRecorder()
+			req := httptest.NewRequest("", "/", nil)
 
-// 			ExpectedProblem: Problem{
-// 				Type:   "https://www.rfc-editor.org/rfc/rfc9110.html#name-200-ok",
-// 				Status: 200,
-// 				Title:  "200 OK",
-// 			},
-// 			ExpectedContentType: "application/problem+json",
-// 			ExpectedStatusCode:  200,
-// 		},
-// 		{
-// 			Name: "Fail_ProblemWithBadStatusCode_99",
-// 			InputProblem: Problem{
-// 				Status: 99,
-// 			},
+			ServeJSON(tc.InputProblem).ServeHTTP(recorder, req)
 
-// 			ExpectedProblem: Problem{
-// 				Status: 99,
-// 			},
-// 			ExpectedContentType: "application/problem+json",
-// 			ExpectedStatusCode:  99,
-// 		},
-// 	}
-// 	for _, tc := range testCases {
-// 		t.Run(tc.Name, func(t *testing.T) {
-// 			wr := &responseWriter{}
-// 			tc.InputProblem.Send(wr)
-// 			if wr.header.Get("Content-Type") != tc.ExpectedContentType {
-// 				t.Errorf("unexpecte Content-Type header, expected %q got %q", tc.ExpectedContentType, wr.header.Get("Content-Type"))
-// 			}
-// 			if wr.statusCode != tc.ExpectedStatusCode {
-// 				t.Errorf("unexpected status code, expected '%d' got '%d'", tc.ExpectedStatusCode, wr.statusCode)
-// 			}
-// 			p := Problem{}
-// 			if err := json.NewDecoder(wr).Decode(&p); err != nil {
-// 				t.Errorf("unexpected error during JSON unmarshalling, got following error: '%v'", err)
-// 			}
-// 			if !compareProblems(p, tc.ExpectedProblem) {
-// 				t.Errorf("unexpected Problem value, expected '%v' got '%v'", tc.ExpectedProblem, p)
-// 			}
-// 		})
-// 	}
-// }
+			if recorder.Body.String() != tc.ExpectedJSON {
+				t.Errorf("expected %s, got %s", tc.ExpectedJSON, recorder.Body.String())
+			}
+
+			// TODO: See above, find a way to compact a XML document.
+			//
+			// recorder = httptest.NewRecorder()
+			//
+			// ServeXML(tc.InputProblem).ServeHTTP(recorder, req)
+			//
+			// if recorder.Body.String() != tc.ExpectedXML {
+			// 	t.Errorf("expected %s, got %s", tc.ExpectedJSON, recorder.Body.String())
+			// }
+		})
+	}
+}
