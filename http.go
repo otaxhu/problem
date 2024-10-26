b8ea48f5544ef19d60f81c5811829aa567a6e7bb
otaxhu <oscarperniamoreno@gmail.com>
2024-10-26 11:29:42 -0400

    fix: optimization on HTTP helpers, returning a struct instead of a http.HandlerFunc (closure)

--- http.go
+++ http.go
@@ -7,6 +7,40 @@ import (
 	"strconv"
 )
 
+type problemHTTPWrapper struct {
+	p Problem
+
+	contentType string
+}
+
+func (p *problemHTTPWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	h := w.Header()
+
+	switch p.contentType {
+	case problemJsonContentType:
+		b, err := json.Marshal(p.p)
+		if err != nil {
+			return
+		}
+		h.Set("Content-Type", problemJsonContentType)
+		h.Set("X-Content-Type-Options", "nosniff")
+		h.Set("Content-Length", strconv.Itoa(len(b)))
+		w.WriteHeader(p.p.GetStatus())
+		w.Write(b)
+	case problemXmlContentType:
+		b, err := xml.Marshal(p.p)
+		if err != nil {
+			return
+		}
+		h.Set("Content-Type", problemXmlContentType)
+		h.Set("X-Content-Type-Options", "nosniff")
+		h.Set("Content-Length", strconv.Itoa(len(xml.Header)+len(b)))
+		w.WriteHeader(p.p.GetStatus())
+		w.Write(append([]byte(xml.Header), b...))
+	}
+}
+
 // ServeXML returns a Handler that serves the p argument in XML format.
 //
 // p MUST not be a [MapProblem], since it cannot be marshaled to XML.
@@ -14,15 +48,10 @@ import (
 // Headers Content-Type is set to 'application/problem+xml' and X-Content-Type-Options is set to 'nosniff';
 // and finally writes the status code from p.GetStatus().
 func ServeXML(p Problem) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h := w.Header()
-		b, _ := xml.Marshal(p)
-		h.Set("Content-Type", problemXmlContentType)
-		h.Set("X-Content-Type-Options", "nosniff")
-		h.Set("Content-Length", strconv.Itoa(len(xml.Header)+len(b)))
-		w.WriteHeader(p.GetStatus())
-		w.Write(append([]byte(xml.Header), b...))
-	})
+	return &problemHTTPWrapper{
+		p:           p,
+		contentType: problemXmlContentType,
+	}
 }
 
 // ServeJSON returns a Handler that serves the p argument in JSON format
@@ -30,13 +59,8 @@ func ServeXML(p Problem) http.Handler {
 // Headers Content-Type is set to 'application/problem+json' and X-Content-Type-Options is set to
 // 'nosniff'; and finally writes the status code from p.GetStatus().
 func ServeJSON(p Problem) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h := w.Header()
-		b, _ := json.Marshal(p)
-		h.Set("Content-Type", problemJsonContentType)
-		h.Set("X-Content-Type-Options", "nosniff")
-		h.Set("Content-Length", strconv.Itoa(len(b)))
-		w.WriteHeader(p.GetStatus())
-		w.Write(b)
-	})
+	return &problemHTTPWrapper{
+		p:           p,
+		contentType: problemJsonContentType,
+	}
 }
